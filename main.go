505311b9ca632f806b73054234267adefc4abe89
Kamil <kamil.raxmatullin.01@gmail.com>
2025-08-02 10:46:11 +0300

    edit go.mod file requirements

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/kamilrahmatullin/restaurant-management/env"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func main() {
 	port := env.GetValue("PORT", "8000")
+
+	router := gin.new()
+	router.Use(gin.Logger())
 }
