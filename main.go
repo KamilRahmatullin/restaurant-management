9c3c2a99152a2a04580c15fe07bc07db4d9860d7
Kamil <kamil.raxmatullin.01@gmail.com>
2025-08-02 10:47:01 +0300

    fixed some error

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/kamilrahmatullin/restaurant-management/env"
+	"github.com/kamilrahmatullin/restaurant-management/routes"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func main() {
 	port := env.GetValue("PORT", "8000")
 
-	router := gin.new()
+	router := gin.New()
 	router.Use(gin.Logger())
+	routes.UserRoutes(router)
 }
