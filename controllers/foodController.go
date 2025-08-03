31d88cec9f33b98a680d4cc97dd76f973272ecd0
Kamil <kamil.raxmatullin.01@gmail.com>
2025-08-03 16:17:08 +0300

    implement get food handler

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,13 +1,38 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kamilrahmatullin/restaurant-management/database"
+	"github.com/kamilrahmatullin/restaurant-management/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc {
 	return func(ctx *gin.Context) {}
 }
 
 func GetFood() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(ctx *gin.Context) {
+		c, cancel := context.WithTimeout(context.Background(), time.Second*30)
+		defer cancel()
+
+		foodId := ctx.Param("food_id")
+
+		food := models.Food{}
+
+		if err := foodCollection.FindOne(c, bson.M{"food_id": foodId}).Decode(&food); err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "error occured while fetching the food item"})
+		}
+
+		ctx.JSON(http.StatusOK, food)
+	}
 }
 
 func CreateFood() gin.HandlerFunc {
