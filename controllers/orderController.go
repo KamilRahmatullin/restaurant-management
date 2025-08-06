4b58b29f55616974c5a34969da76dc50bc63852f
Kamil <kamil.raxmatullin.01@gmail.com>
2025-08-06 12:31:52 +0300

    update order controller

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -10,9 +11,12 @@ import (
 	"github.com/kamilrahmatullin/restaurant-management/database"
 	"github.com/kamilrahmatullin/restaurant-management/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var orderCollection = database.OpenCollection(database.Client, "order")
+var tableCollection = database.OpenCollection(database.Client, "table")
 
 func GetOrders() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -57,5 +61,57 @@ func CreateOrder() gin.HandlerFunc {
 }
 
 func UpdateOrder() gin.HandlerFunc {
-	return func(ctx *gin.Context) {}
+	return func(ctx *gin.Context) {
+		c, cancel := context.WithTimeout(context.Background(), time.Second*30)
+		defer cancel()
+
+		order := models.Order{}
+		table := models.Table{}
+
+		var updateObj primitive.D
+
+		orderId := ctx.Param("order_id")
+
+		if err := ctx.BindJSON(&order); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if order.Table_id != nil {
+			if err := tableCollection.FindOne(c, bson.M{"table_id": order.Table_id}).Decode(&table); err != nil {
+				msg := fmt.Sprintf("Table was not found")
+				ctx.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+				return
+			}
+
+			updateObj = append(updateObj, bson.E{Key: "table_id", Value: order.Table_id})
+		}
+
+		order.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		updateObj = append(updateObj, bson.E{Key: "updated_at", Value: order.Updated_at})
+
+		upsert := true
+		filter := bson.M{"order_id": orderId}
+
+		opt := options.UpdateOptions{
+			Upsert: &upsert,
+		}
+
+		result, err := orderCollection.UpdateOne(
+			c,
+			filter,
+			bson.D{
+				{Key: "$set", Value: updateObj},
+			},
+			&opt,
+		)
+
+		if err != nil {
+			msg := fmt.Sprintf("order item update failed")
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, result)
+	}
 }
